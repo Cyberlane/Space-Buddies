0fa6d77c8110e11d38bec6b916cde569226d4f7e
AndyM84 <amale@zibings.com>
2014-05-31 16:31:13 -0400

    Refactoring scales and structures

--- Space-Buddies.ino
+++ Space-Buddies.ino
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_BARS 50
+#define NUM_PLAYERS 2
+
 #define tone_C 1911
 #define tone_C1 1804
 #define tone_D 1703
@@ -22,47 +28,60 @@
 #define tone_a 568
 #define tone_bb 536
 #define tone_b 506
-#define tone_p 0  //pause
+#define tone_p -1  //pause
+#define tone_null 0
 
+typedef enum
+{
+	A_MINOR,
+	E_MINOR,
+	B_MINOR,
+	F_SHARP,
+	C_SHARP,
+	G_SHARP,
+	E_FLAT,
+	B_FLAT,
+	F_MINOR,
+	C_MINOR,
+	G_MINOR,
+	D_MINOR
+} scale_t;
 
 typedef struct _note_
 {
-  int tone;
-  int duration;
+	int tone;
+	int duration;
 } note;
 
-typedef struct _bar_
+typedef struct _player_
 {
-  note* notes[];
-} bar;
+	note notes[MAX_BARS * 24];
+	int curr_duration;
+} player;
 
-typedef struct _tune_
-{
-  bar* bars[];
-} tune;
-
-
+player players[NUM_PLAYERS];
+int count_types[3] = { 4, 3, 2 };
 
 // mario
-int mario_m[] = {tone_e, tone_e, tone_e, tone_c, tone_e, tone_g, tone_G, tone_c, tone_G, tone_E, tone_A, tone_B, tone_Bb, tone_A, tone_G, tone_e, tone_g, tone_a, tone_f, tone_g, tone_e, tone_c, tone_d, tone_B, tone_c};
-int mario_r[] = {6, 12, 12, 6, 12, 24, 24, 18, 18, 18, 12, 12, 6, 12, 8, 8, 8, 12, 6, 12, 12, 6, 6, 6, 12};
+int mario_m[] = { tone_e, tone_e, tone_e, tone_c, tone_e, tone_g, tone_G, tone_c, tone_G, tone_E, tone_A, tone_B, tone_Bb, tone_A, tone_G, tone_e, tone_g, tone_a, tone_f, tone_g, tone_e, tone_c, tone_d, tone_B, tone_c };
+int mario_r[] = { 6, 12, 12, 6, 12, 24, 24, 18, 18, 18, 12, 12, 6, 12, 8, 8, 8, 12, 6, 12, 12, 6, 6, 6, 12 };
 
-int a_minor[] = {tone_A,tone_a,tone_B,tone_b,tone_C,tone_c,tone_D,tone_d,tone_E,tone_e,tone_F,tone_f,tone_E,tone_e,tone_F,tone_f,tone_G,tone_g};
-int e_minor[] = {tone_E,tone_e,tone_F1,tone_f1,tone_G,tone_g,tone_A,tone_a,tone_B,tone_b,tone_C,tone_c,tone_D,tone_d};
-int b_minor[] = {tone_B,tone_b,tone_C1,tone_c1,tone_D,tone_d,tone_E,tone_e,tone_F1,tone_f1,tone_G,tone_g,tone_A,tone_a};
-int f_sharp[] = {tone_F1,tone_f1,tone_Ab,tone_ab,tone_A,tone_a,tone_B,tone_b,tone_C1,tone_c1,tone_D,tone_d,tone_E,tone_e};
-int c_sharp[] = {tone_C1,tone_c1,tone_Eb,tone_eb,tone_E,tone_e,tone_F1,tone_f1,tone_A,tone_a,tone_B,tone_b};
-int g_sharp[] = {tone_Eb,tone_eb,tone_Bb,tone_bb,tone_B,tone_b,tone_C1,tone_c1,tone_Eb,tone_eb,tone_E,tone_e,tone_F1,tone_f1};
-int e_flat[] = {tone_Eb,tone_eb,tone_F,tone_f,tone_F1,tone_f1,tone_Ab,tone_ab,tone_Bb,tone_bb,tone_C,tone_c,tone_F,tone_f};
-int b_flat[] = {tone_Bb,tone_bb,tone_C,tone_c,tone_C1,tone_c1,tone_Eb,tone_eb,tone_F,tone_f,tone_F1,tone_f1,tone_Ab,tone_ab};
-int f_minor[] = {tone_F,tone_f,tone_G,tone_g,tone_Ab,tone_ab,tone_Bb,tone_bb,tone_C,tone_c,tone_C1,tone_c1,tone_Eb,tone_eb};
-int c_minor[] = {tone_C,tone_c,tone_D,tone_d,tone_Eb,tone_eb,tone_F,tone_f,tone_G,tone_g,tone_Ab,tone_ab,tone_Bb,tone_bb};
-int g_minor[] = {tone_G,tone_g,tone_A,tone_a,tone_Bb,tone_bb,tone_C,tone_c,tone_D,tone_d,tone_Eb,tone_eb,tone_F,tone_f};
-int d_minor[] = {tone_D,tone_d,tone_E,tone_e,tone_F,tone_f,tone_G,tone_g,tone_A,tone_a,tone_Bb,tone_bb,tone_C,tone_c};
+int scales[12][] = {
+	{ tone_A, tone_a, tone_B, tone_b, tone_C, tone_c, tone_D, tone_d, tone_E, tone_e, tone_F, tone_f, tone_E, tone_e, tone_F, tone_f, tone_G, tone_g }, // a minor
+	{ tone_E, tone_e, tone_F1, tone_f1, tone_G, tone_g, tone_A, tone_a, tone_B, tone_b, tone_C, tone_c, tone_D, tone_d }, // e minor
+	{ tone_B, tone_b, tone_C1, tone_c1, tone_D, tone_d, tone_E, tone_e, tone_F1, tone_f1, tone_G, tone_g, tone_A, tone_a }, // b minor
+	{ tone_F1, tone_f1, tone_Ab, tone_ab, tone_A, tone_a, tone_B, tone_b, tone_C1, tone_c1, tone_D, tone_d, tone_E, tone_e }, // f sharp
+	{ tone_C1, tone_c1, tone_Eb, tone_eb, tone_E, tone_e, tone_F1, tone_f1, tone_A, tone_a, tone_B, tone_b }, // c sharp
+	{ tone_Eb, tone_eb, tone_Bb, tone_bb, tone_B, tone_b, tone_C1, tone_c1, tone_Eb, tone_eb, tone_E, tone_e, tone_F1, tone_f1 }, // g sharp
+	{ tone_Eb, tone_eb, tone_F, tone_f, tone_F1, tone_f1, tone_Ab, tone_ab, tone_Bb, tone_bb, tone_C, tone_c, tone_F, tone_f }, // e flat
+	{ tone_Bb, tone_bb, tone_C, tone_c, tone_C1, tone_c1, tone_Eb, tone_eb, tone_F, tone_f, tone_F1, tone_f1, tone_Ab, tone_ab }, // b flat
+	{ tone_F, tone_f, tone_G, tone_g, tone_Ab, tone_ab, tone_Bb, tone_bb, tone_C, tone_c, tone_C1, tone_c1, tone_Eb, tone_eb }, // f minor
+	{ tone_C, tone_c, tone_D, tone_d, tone_Eb, tone_eb, tone_F, tone_f, tone_G, tone_g, tone_Ab, tone_ab, tone_Bb, tone_bb }, // c minor
+	{ tone_G, tone_g, tone_A, tone_a, tone_Bb, tone_bb, tone_C, tone_c, tone_D, tone_d, tone_Eb, tone_eb, tone_F, tone_f }, // g minor
+	{ tone_D, tone_d, tone_E, tone_e, tone_F, tone_f, tone_G, tone_g, tone_A, tone_a, tone_Bb, tone_bb, tone_C, tone_c } // d minor
+};
 
-int* scales[] = {a_minor,e_minor,b_minor,f_sharp,c_sharp,g_sharp,e_flat,b_flat,f_minor,c_minor,g_minor,d_minor};
-
-int durations[] = {2,4,6,8,12,16,24};
+int durations[] = { 2, 4, 6, 8, 12, 16, 24 };
  
 const int PIEZO = 6;
  
